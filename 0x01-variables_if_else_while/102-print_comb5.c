73e860564960479f639efc9dd12d725fc0d7eacc
FrezerDaniel <danielfrezer@gmail.com>
2023-02-19 22:53:27 +0000

    Variables

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main -main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 0; i < 10 ; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (k = 0; k < 10; k++)
+			{
+				for (l = 0; l < 10; l++)
+				{
+					if (i != k && j != l)
+					{
+						putchar(i + '0');
+						putchar(j + '0');
+						putchar(' ');
+						putchar(k + '0');
+						putchar(l + '0');
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
